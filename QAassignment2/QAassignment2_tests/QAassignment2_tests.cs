ba73acfbfa66b337fde120b5df042741b6bc32cb
makarenkoA <Amakarenko1466@conestogac.on.ca>
2019-02-11 12:47:42 -0500

    Created QAassignment2 project

---
+++ QAassignment2/QAassignment2_tests/QAassignment2_tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QAassignment2_tests
+{
+    class QAassignment2_tests
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
